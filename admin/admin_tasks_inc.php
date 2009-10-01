5883c30a64ba116656d6380258c3f4158c0c2c32
wjames5 <will@tekimaki.com>
2009-10-01 14:17:05 +0000

    remove ref to non-existant copyright.txt file

--- admin/admin_tasks_inc.php
+++ admin/admin_tasks_inc.php
@@ -1,8 +1,8 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_tasks/admin/admin_tasks_inc.php,v 1.2 2009/10/01 13:45:49 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_tasks/admin/admin_tasks_inc.php,v 1.3 2009/10/01 14:17:05 wjames5 Exp $
 
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 include_once( TASKS_PKG_PATH.'Tasks.php' );
