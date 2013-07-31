6628eb7ac363a4df50386fd0ed3bf6d7a5164bb5
lsces <lester@lsces.co.uk>
2013-07-31 23:03:51 +0100

    phpdoc tidy

--- load_history.php
+++ load_history.php
@@ -4,9 +4,14 @@
  *
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
+ *
+ * @package tasks
+ * @subpackage functions
  */
 
-// Initialization
+/**
+ * Initialization
+ */
 require_once( '../kernel/setup_inc.php' );
 require_once( TASKS_PKG_PATH.'Tasks.php' );
 
